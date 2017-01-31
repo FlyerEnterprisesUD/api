0991fb445915569e0afe104fe091572370ef3218
Ajay Patnaik <ajaypatnaik@Ajays-MacBook-Pro.local>
2017-01-31 16:16:56 -0500

    Route Setups
    Route Setups for promotions, menus and about for the chill.

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', function(req, res){
   res.send('API');
+});
+
+app.get('/chill-menu', function(req, res){
+  res.sendfile('./menus/chill.json');
+});
+
+app.get('/chill-about', function(req, res){
+  res.sendfile('./about/chill.json');
+});
+
+app.get('/promotions-general', function(req, res){
+  res.sendfile('./promotions/general.json');
 });
 
 app.listen(PORT);
